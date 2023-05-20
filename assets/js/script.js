7b1c7309582ffa6bd165f78384cc3824100ad8ff
Valeriia Shybika <91203182+sheiby13@users.noreply.github.com>
2023-05-21 00:31:36 +0300

    Popup
    implementation of popup | restaurant discount

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,3 +160,39 @@ window.addEventListener("mousemove", function (event) {
     parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
   }
 });
+
+
+/*** Pop UP*/
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+const closeBtn = document.querySelector('.modal__close-btn');
+const discountBtn = document.querySelector('.modal__button');
+// ---- ---- cookies ---- ---- //
+const createCookie = () => {
+  let maxAge = ';max-age=10';
+  let path = ';path=/';
+  document.cookie = 'modalpopup=displayed' + maxAge + path;
+};
+
+// ---- ---- add active and cookie ---- ---- //
+const displayModal = () => {
+  if (document.cookie.indexOf('modalpopup') == -1) {
+    modal.classList.add('active');
+    modalOverlay.classList.add('active');
+    createCookie();
+  }
+};
+// Виклик функції displayModal через 2 секунди
+setTimeout(() => {
+  displayModal();
+}, 2000);
+// Приховання спливаючого вікна при натисканні на кнопку закриття або знижку
+// ---- ---- remove active ---- ---- //
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  modalOverlay.classList.remove('active');
+});
+discountBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  modalOverlay.classList.remove('active');
+});
